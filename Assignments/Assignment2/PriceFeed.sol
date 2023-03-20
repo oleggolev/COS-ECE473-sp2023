4ee2315f08dec39f262028d3d05113320de7036f
Oleg Golev <ogolev@princeton.edu>
2023-03-20 17:03:13 -0400

    assignment 2 restored

--- Assignments/Assignment2/PriceFeed.sol
+++ Assignments/Assignment2/PriceFeed.sol
@@ -1,9 +1,31 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.0 <0.9.0;
 
-import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 import "./interfaces/IPriceFeed.sol";
+import "./interfaces/AggregatorV3Interface.sol";
 
+// Price feed smart contract
 contract PriceFeed is IPriceFeed {
-    /* TODO: implement your functions here */
+    AggregatorV3Interface internal priceFeed;
+
+    /**
+     * Creates an aggregator for the specified address on the Goerli testnet.
+     */
+    constructor(address priceFeedAddress) {
+        priceFeed = AggregatorV3Interface(priceFeedAddress);
+    }
+
+    /**
+     * Returns the latest price.
+     */
+    function getLatestPrice() external view override returns (int256, uint256) {
+        (
+            /* uint80 roundID */,
+            int256 price,
+            /* uint256 startedAt */,
+            uint256 timestamp,
+            /* uint80 answeredInRound */
+        ) = priceFeed.latestRoundData();
+        return (price, timestamp);
+    }
 }
